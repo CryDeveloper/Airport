0b05a971081ca412c687f6522e2eef7efc1bb2fc
CryDeveloper <fordefdevat@gmail.com>
2023-04-29 04:47:30 +0300

    добавлены все страницы и готова странциы рейсов

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Airport.StaticClasses;
+using Airport.Pages;
+
+namespace Airport
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.ActiveFrame = frame;
+            MainFrame.ActiveFrame.Navigate(new PageFlights());
+        }
+
+        private void btnFlights_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.ActiveFrame.Navigate(new PageFlights());
+        }
+
+        private void btnPassangers_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.ActiveFrame.Navigate(new Passangers());
+        }
+
+        private void btnRoutes_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.ActiveFrame.Navigate(new Routes());
+        }
+        private void btnPlane_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.ActiveFrame.Navigate(new Plane());
+        }
+
+        private void btnBoked_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.ActiveFrame.Navigate(new BookedTickets());
+        }
+
+    }
+}
